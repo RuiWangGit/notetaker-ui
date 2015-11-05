49e7e14a03786ba462d6fc16fc3e4d0944d5b22b
Mathew Thomas <mattazoid@gmail.com>
2015-11-04 22:35:55 -0500

    updates

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
 
+const PORT=9090;
+
 var options = {
   router: {
     'localhost/api': '127.0.0.1:8080',
-    'localhost': '127.0.0.1:8000'
+    'localhost': '127.0.0.1:8080'
   }
 };
 
 var proxyServer = httpProxy.createServer(options);
-proxyServer.listen(9090);
+proxyServer.listen(PORT, function() {
+    console.log("Proxy server listening on: http://localhost:%s", PORT);
+});
